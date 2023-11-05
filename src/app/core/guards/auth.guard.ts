7231b25791e19d3fd0b41d44e42f226689154653
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2023-11-05 16:42:08 -0500

    Add authguard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,13 +1,17 @@
 import { CanActivateFn, Router } from '@angular/router';
 import {inject} from "@angular/core";
 import {AuthService} from "../services/auth.service";
-import {MessageService} from "../services/message.service";
 import {tap} from "rxjs";
-
 
 export const authGuard: CanActivateFn = () => {
 
   const authService: AuthService = inject(AuthService);
 
-  return authService.isAuth$()
+  return authService.isAuth$().pipe(
+    tap((isAuth) => {
+      if (!isAuth) {
+        inject(Router).navigate(['/login']);
+      }
+    }),
+  );
 };
--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
 import {RegisterComponent} from "./components/register/register.component";
+import {authGuard} from "../../core/guards/auth.guard";
 
 const routes: Routes = [
   {
@@ -12,12 +13,14 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
-    title: 'Login'
+    title: 'Login',
+    // canActivate: [authGuard]
   },
   {
     path: 'register',
     component: RegisterComponent,
-    title: 'Register'
+    title: 'Register',
+    // canActivate: [authGuard]
   },
 ];
 
