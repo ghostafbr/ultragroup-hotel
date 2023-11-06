207bd5e1020316b459cf554f027df945b579417b
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2023-11-05 19:13:59 -0500

    Delete unused component

--- src/app/modules/guest/hotel/components/hotel-reservation/hotel-reservation.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-hotel-reservation',
-  templateUrl: './hotel-reservation.component.html'
-})
-export class HotelReservationComponent {
-
-}
